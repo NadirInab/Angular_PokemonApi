9f5f9683bb42849eba3d9ce928c8f8d5b405433b
nadir inab <nadir.inab.dev@gmail.com>
2023-12-04 01:21:58 +0100

    routinf is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PokeInfoComponent } from './components/poke-info/poke-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path :"", 
+    component : HomeComponent, 
+    title : 'Home Page'
+  }, 
+  {
+    path : 'info', 
+    component : PokeInfoComponent, 
+    title : "Poke info"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
